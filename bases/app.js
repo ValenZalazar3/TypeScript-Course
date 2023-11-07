b2019ac55bec136af439586ef7c0837ef60925f1
Valen <valenzalazar3@gmail.com>
2023-11-07 10:15:33 -0300

    improve typescript

--- bases/app.js
+++ bases/app.js
@@ -1,2 +1,9 @@
-var msg = 'Hello World';
+"use strict";
+const msg = 'Hello World';
 console.log(msg);
+const hero = {
+    name: 'Tony Stark',
+    age: 45
+};
+hero.age = 50;
+console.log(hero.age + 1);
