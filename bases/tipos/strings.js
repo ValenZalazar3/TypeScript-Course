adc4a80ffa6e49cb2798ce179b37c646740ee4e9
Valen <valenzalazar3@gmail.com>
2023-11-21 12:54:50 -0300

    improve typeScript

--- bases/tipos/strings.js
+++ bases/tipos/strings.js
@@ -1,1 +1,9 @@
 "use strict";
+;
+(() => {
+    var _a;
+    const batman = 'Batman';
+    const linternaVerde = 'Linterna Verde';
+    const volcanNegro = `Héroe: Volcan Negro`;
+    console.log(((_a = batman[10]) === null || _a === void 0 ? void 0 : _a.toUpperCase()) || 'No esta');
+})();
