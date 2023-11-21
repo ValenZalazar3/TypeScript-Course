ca79db4dab8a32533da7b2b19089935862c8452e
Valen <valenzalazar3@gmail.com>
2023-11-21 12:54:29 -0300

    improve typeScript

---
+++ bases/tipos/any.js
@@ -0,0 +1,13 @@
+"use strict";
+;
+(() => {
+    let avengers = 123;
+    let exists;
+    let power;
+    avengers = 'Dr. Strange';
+    console.log(avengers.charAt(0));
+    console.log(avengers.charAt(0)); // as string para convertirlo a string y tener la ayuda que da
+    avengers = 150.2325123;
+    console.log(avengers.toFixed(2));
+    // con any perdes toda ayuda de tipado
+})();
